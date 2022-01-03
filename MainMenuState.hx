02826b6595b557fb0b61a9b6da1c43992c70b005
IsaackyOFC <96953830+IsaackyOFC@users.noreply.github.com>
2022-01-03 20:15:38 -0300

    Add files via upload

--- MainMenuState.hx
+++ MainMenuState.hx
@@ -1,4 +1,4 @@
-﻿package;
+package;
 
 #if desktop
 import Discord.DiscordClient;
@@ -113,10 +113,10 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
+			menuItem.scale.set(0.8 , 0.8);
 			menuItem.ID = i;
-			//menuItem.screenCenter(X);
-			menuItem.x +=250;
-            menuItems.add(menuItem);
+			menuItem.screenCenter(X);
+			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
 			menuItem.scrollFactor.set(0, scr);
@@ -266,7 +266,8 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			//spr.screenCenter(X);
+			spr.screenCenter(X);
+			spr.x += -300;
 		});
 	}
 
