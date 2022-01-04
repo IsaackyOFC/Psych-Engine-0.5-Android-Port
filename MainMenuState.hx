fcd21dfc49fa5b7e927e429bec18a65a3a27cd35
IsaackyOFC <96953830+IsaackyOFC@users.noreply.github.com>
2022-01-03 21:15:30 -0300

    Add files via upload

--- MainMenuState.hx
+++ MainMenuState.hx
@@ -115,7 +115,8 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.play('idle');
 			menuItem.scale.set(0.8 , 0.8);
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
+			//menuItem.screenCenter(X);
+			menuItem.x += 100;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
@@ -266,8 +267,7 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			spr.screenCenter(X);
-			spr.x += -300;
+			//spr.screenCenter(X);
 		});
 	}
 
